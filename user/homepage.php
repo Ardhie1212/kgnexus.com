529b4aab3d975d655f412e7fdd83acaefe8be523
Muhammad Ardhie <m.ardhie5325@gmail.com>
2024-05-03 00:45:03 +0700

    Dashboard navbar

--- user/homepage.php
+++ user/homepage.php
@@ -5,16 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KGNexus</title>
+    <link rel="stylesheet" href="../style/homepage.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <body>
     <!-- Navigation Bar and Sidebar -->
     <header>
-        <img src="images/Logo.jpg" alt="" class="Logo">
         <nav class="navbar">
-            <a href="#">Button 1</a>
-            <a href="#">Button 2</a>
-            <a href="#">Button 3</a>
+            <div class="logo">
+                <a href="#">
+                    K G N E X U S
+                </a>
+            </div>
+            <ul class="links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Kategori</a></li>
+                <li><a href="#">Wallet</a></li>
+            </ul>
+            <a href="#"><i class='bx bxs-user-circle'></i></a>
         </nav>
     </header>
 
