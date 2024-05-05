ffef3d8ec883d40dbfa75a9c03b43137cc6e4d47
Muhammad Ardhie <m.ardhie5325@gmail.com>
2024-05-05 10:36:12 +0700

    Navbar Transition

--- user/homepage.php
+++ user/homepage.php
@@ -10,7 +10,8 @@
 </head>
 
 <body>
-    <!-- Navigation Bar and Sidebar -->
+
+    <!-- Navigation Bar -->
     <header>
         <nav class="navbar">
             <i class='bx bx-menu' id="menu"></i>
@@ -27,16 +28,46 @@
             <i class='bx bxs-user-circle'></i>
         </nav>
     </header>
+    <!-- End of Navigation Bar -->
+
+    <!-- Sidebar -->
+    <div class="sidebar" id="sidebar">
+        <ul>
+            <li><a href="#"><i class='bx bx-home-alt-2'></i>Menu 1</a></li>
+            <li><a href="#">Menu 2</a></li>
+            <li><a href="#">Menu 3</a></li>
+            <li><a href="#">Menu 4</a></li>
+        </ul>
+    </div>
+    <!-- End of Sidebar -->
+
+    <!-- JavaScript untuk Header dan Sidebar content -->
+    <script>
+        const menuBtn = document.getElementById('menu');
+        const sidebar = document.getElementById('sidebar');
+        
+
+
+        menuBtn.addEventListener('click', () => {
+            sidebar.classList.toggle('show-sidebar');
+            // Kalau div id="sidebar" punya class 'show-sidebar', maka class dihapus. Jika tidak, maka class 'show-sidebar' akan ditambahkan.
+            menuBtn.classList.toggle('bx-x');
+            // Kalau i dgn id="menu" punya class 'bx-x', class dihapus. Kalau ga punya, class nya ditambah. 
+        });
+    </script>
+    <!-- End of JavaScript Sidebar -->
 
     <!-- Main content -->
     <main>
 
     </main>
+    <!-- End of Main content -->
 
     <!-- Footer -->
     <footer>
 
     </footer>
+    <!-- End of footer -->
 </body>
 
 </html>
