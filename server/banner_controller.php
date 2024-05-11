c97a45fb774e8ab78fa32c7cf796006a0b1ec3bd
Muhammad Ardhie <m.ardhie5325@gmail.com>
2024-05-11 09:41:34 +0700

    Banner Database GET

---
+++ server/banner_controller.php
@@ -0,0 +1,10 @@
+<?php
+    include('connection.php');
+    $query_banner = "SELECT photo1 from game WHERE sector = 'Banner' LIMIT 5";
+
+    $stmt_banner = $conn ->prepare($query_banner);
+
+    $stmt_banner->execute();
+
+    $banner_image = $stmt_banner->get_result();
+?>
